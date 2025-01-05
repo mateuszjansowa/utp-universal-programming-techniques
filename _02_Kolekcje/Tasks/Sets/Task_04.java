5dda15535b8c68cd7071e29a58c80c57a00d12c9
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2025-01-05 13:52:17 +0100

    finish task 04

--- _02_Kolekcje/Tasks/Sets/Task_04.java
+++ _02_Kolekcje/Tasks/Sets/Task_04.java
@@ -1,10 +1,27 @@
 package _02_Kolekcje.Tasks.Sets;
 
-public class Task_04 {
+import java.util.*;
 
+public class Task_04 {
+    public static void main(String[] args) {
+        Set<Employee> set = new HashSet<>(Set.of(
+                new Employee(1, "Mateusz", 12500),
+                new Employee(2, "Maria", 10500),
+                new Employee(3, "Jacek", 17500),
+                new Employee(4, "Michał", 4500)
+        ));
+
+        set.removeIf(employee -> employee.getSalary() < 5000);
+
+        Set<Employee> treeSet = new TreeSet<>(set);
+        // Set<Employee> treeSet = new TreeSet<>(Comparator.comparingDouble(Employee::getSalary).reversed());
+        // treeSet.addAll(set);
+
+        System.out.println(treeSet);
+    }
 }
 
-class Employee {
+class Employee implements Comparable<Employee> {
     private int id;
     private String name;
     private double salary;
@@ -15,12 +32,35 @@ class Employee {
         this.salary = salary;
     }
 
+    public double getSalary() {
+        return salary;
+    }
+
     @Override
     public String toString() {
-        return "Employee{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", salary=" + salary +
-                '}';
+        return String.format("Employee{id=%d, name='%s', salary=%.2f}", id, name, salary);
+    }
+
+    @Override
+    public int compareTo(Employee other) {
+        return Double.compare(other.salary, this.salary);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Employee employee = (Employee) o;
+        return this.id == employee.id && Double.compare(this.salary, employee.salary) == 0 && Objects.equals(this.name, employee.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, salary);
     }
 }
