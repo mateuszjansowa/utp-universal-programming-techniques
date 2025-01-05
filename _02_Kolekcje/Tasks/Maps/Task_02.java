6a5e7d87c31e0cc02c3988a16cd5cc6e1ad93923
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2025-01-05 15:38:44 +0100

    Finish task 03

--- _02_Kolekcje/Tasks/Maps/Task_02.java
+++ _02_Kolekcje/Tasks/Maps/Task_02.java
@@ -1,4 +1,32 @@
 package _02_Kolekcje.Tasks.Maps;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Task_02 {
+    public static void main(String[] args) {
+        Map<String, Integer> words = new HashMap<>();
+
+        try (Scanner in = new Scanner(System.in)) {
+            while (true) {
+                String input = in.nextLine().trim();
+
+                if (input.equalsIgnoreCase("exit")) {
+                    break;
+                }
+
+                String wordsArray[] = input.split("\\s+");
+
+                for (String word : wordsArray) {
+                   words.put(word, words.getOrDefault(word, 0) + 1);
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(words);
+    }
 }
